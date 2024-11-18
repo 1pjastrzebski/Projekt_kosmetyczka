f1c5a876b7a6e941584b9e8301d2707e19ee205a
cojak <113033656+1pjastrzebski@users.noreply.github.com>
2024-11-18 10:20:03 +0100

    Add files via upload

--- logowanie.php
+++ logowanie.php
@@ -1,19 +1,21 @@
+<?php
+    session_start();
+    if(isset($_SESSION['logged'])){
+        header("Location: ../index.php");
+        exit();
+    }
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logowanie</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-        *{
-            padding: 0;
-            margin: 0;
-            box-sizing: border-box;
-            background-color: white;
-        }
+        
         .container{
-            height: 50vh;
+            height: 55vh;
         }
         form{
             border: 1px solid black;
@@ -21,19 +23,26 @@
         .form-floating{
             margin: 15px;
         }
+        .form-error-text{
+            color: tomato;
+        }
     </style>
+    <script src="js/form.js" defer></script>
 </head>
 <body>
-    <div class="container d-flex justify-content-center align-items-center">
+    <div class="container d-flex justify-content-center align-items-center mt-5">
         <form action="php/log.php" method="post" class="p-4 rounded">
             <h2 class="text-center">Zaloguj się</h2>
             <div class="form-floating">
-                <input type="email" class="form-control" name="email" required placeholder="E-mail">
+                <input type="email" class="form-control" id="email" name="email" required placeholder="E-mail">
                 <label for="email">E-mail</label>
             </div>
             <div class="form-floating">
-                <input type="password" class="form-control" name="password" required placeholder="Hasło">
+                <input type="password" class="form-control" id="password" name="password" required placeholder="Hasło">
                 <label for="password">Hasło</label>
+            </div>
+            <div class="d-flex justify-content-center">
+                <button class="btn" type="submit">Zaloguj się</button>
             </div>
         </form>
     </div>
---
+++ php/log.php
@@ -0,0 +1,36 @@
+<?php
+    session_start()
+    require_once "../components/connect.php";
+    $conn = new mysqli($host,$username,$password,$database);
+    if($conn->connect_error){
+        die("Błąd połączenia " . $conn->connect_error);
+    }
+    else{
+        if(isset($_POST["email"]) && isset($_POST["password"])){
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $sql = "SELECT * FROM klienci Where email='%s'";
+            if($kwerenda = $conn->query($sql)){
+                if($kwerenda->num_rows == 1){
+                    $row = $kwerenda->fetch_assoc();
+                    if(password_verify($password, $row['password'])){
+                        $_SESSION["user"] = $row;   
+                        unset($_SESSION["log_err"]);
+                        $_SESSION["logged"] = true;
+                        $kwerenda->close();
+                        header("Location: ../index.php");
+                    }else{
+                        $_SESSION['log_err'] = "Nieprawidłowe hasło";
+                        header("Location: ../logowanie.php");
+                    }
+                }else{
+                    $_SESSION["log_err"] = "Nieprawidłowy email";
+                    header("Location: ../logowanie.php");
+                }
+            }
+        }else{
+            header("Location: ../logowanie.php");
+        }
+        $conn->close();
+    }
+?>
