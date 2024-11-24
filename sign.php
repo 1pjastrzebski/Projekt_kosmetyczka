0459becae5f449795339af78697a5cc48996d026
Vladislavius17 <114662177+Vladislavius17@users.noreply.github.com>
2024-11-24 18:17:06 +0100

    Add files via upload

---
+++ sign.php
@@ -0,0 +1,42 @@
+<?php
+
+session_start();
+
+require_once "../components/connect.php";
+
+
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Błąd połączenia " . $conn->connect_error);
+} else {
+    if (isset($_POST["email"]) && isset($_POST["haslo"])) {
+        $imie = $_POST["Imie"];
+        $nazwisko = $_POST["Nazwisko"];
+        $email = $_POST["email"];
+        $haslo = $_POST["haslo"];
+        $phaslo = $_POST["phaslo"];
+        $tel = $_POST["tel"];
+        $hHaslo = password_hash($haslo, PASSWORD_DEFAULT);
+        $sql = "SELECT email FROM klienci WHERE email='$email'";
+        if ($kwerenda = $conn->query($sql)) {
+            if ($kwerenda->num_rows > 0) {
+                $_SESSION["email_err"] = "Istnieje już konto z tym adresem email";
+                header('Location: ../Stworz_konto.php');
+                $conn->close();
+                exit();
+            }
+        } else {
+            header("Location: ../sign-form.php");
+        }
+        $sql = "INSERT into klienci VALUES(null,'$imie','$nazwisko','$email','$hHaslo','$tel',CURRENT_DATE())";
+        if ($kwerenda = $conn->query($sql)) {
+            $_SESSION['rejestracja'] = true;
+            header("Location: ../index.php");
+        } else {
+            header("Location: ../sign-form.php");
+        }
+    }
+}
+$conn->close();
---
+++ sing-form.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rejestracja</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        body {
+            height: 55vh;
+        }
+
+        main {
+            padding: 5px 50px 30px 50px;
+            border: 2px solid black;
+        }
+
+        .form-body {
+            display: flex;
+            flex-direction: row;
+
+        }
+
+        .fdiv {
+            display: flex;
+            flex-direction: column;
+            margin: 0 10px;
+        }
+
+        .form-floating {
+            margin: 10px 2px;
+        }
+        .form-error-text{
+            color: tomato;
+        }
+        button{
+            margin: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container d-flex align-items-center flex-column">
+        <main class="d-flex flex-column align-items-center p-4 rounded mt-5">
+            <h1>Stwórz konto</h1>
+            <form method="post" action="php/sign.php">
+                <div class="form-body">
+
+                    <div class="fdiv">
+                        <div class="form-floating">
+                            <input type="text" name="Imie" required maxlength="50" class="form-control" placeholder="Imie">
+                            <label for="Imie">Imie</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="text" name="Nazwisko" required maxlength="50" class="form-control" placeholder="Nazwisko">
+                            <label for="Nazwisko">Nazwisko</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="email" name="email" required maxlength="100" class="form-control" placeholder="Email">
+                            <label for="email">Email</label>
+                        </div>
+
+                    </div>
+                    <div class="fdiv">
+                        <div class="form-floating">
+                            <input type="password" id="haslo" name="haslo" required maxlength="50" class="form-control" placeholder="Haslo">
+                            <label for="haslo">Hasło</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="password" id="phaslo" name="phaslo" required maxlength="50" class="form-control" placeholder="Powtórz hasło">
+                            <label for="phaslo">Powtórz hasło</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="tel" name="tel" required maxlength="15" placeholder="tel (123-456-789)" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" class="form-control">
+                            <label for="tel">tel (123-456-789)</label>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-dark">Stwórz konto</button>
+
+                </div>
+            </form>
+            <?php
+            if (isset($_SESSION["email_err"])) {
+                echo "<p id='log_err' class='form-error-text'>" . $_SESSION["email_err"] . "</p>";
+                unset($_SESSION["email_err"]);
+            }
+            ?>
+            <div>
+                <p>masz już konto? <a href="log-form.php">Zaloguj się.</a></p>
+            </div>
+        </main>
+    </div>
+    <script src="js/form.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
